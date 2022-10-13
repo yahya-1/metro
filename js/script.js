11b7ca7d77327c3a5637496bf9f30fe45d244df5
yahya-1 <y.a.m.k123456@hotmail.com>
2022-10-13 15:37:21 +0300

    Create a Metro landing page for Dash using HTML5, CSS3 and Bootstrap

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// let optMenu = document.querySelector(".selector"),
+//   selectBtn = document.querySelector(".select-field"),
+//   sBtn_text = document.getElementById("selectText"),
+//   options = document.querySelectorAll("ul .options");
+
+// selectBtn.addEventListener("click", () => {
+//   optMenu.classList.toggle("active");
+// });
+
+// options.forEach((option) => {
+//   option.addEventListener("click", () => {
+//     let selectedOption = option.querySelector(".options p").innerText;
+//     sBtn_text.innerText = selectedOption;
+//     optMenu.classList.remove("active");
+//   });
+// });
